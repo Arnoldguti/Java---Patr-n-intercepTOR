ab715a5fe11f4e49c602a9f2fa65d3f5d3310b7c
Arnoldguti <arnolr.gutierrez@outlook.com>
2017-07-28 20:43:31 +0200

    Add files via upload

---
+++ src/Core/Constants.java
@@ -0,0 +1,25 @@
+package Core;
+
+public interface Constants {
+
+	//Label
+	static final String  ETIQUETA1 = "Apagado";
+	static final String  ETIQUETA2 = "Encendido";
+	static final String ONFORSPEED = "Acelerando";
+	
+	//Buttons
+	static final String BUTTON_CONTENT_1 = "Encender";
+	static final String BUTTON_CONTENT_2 = "Apagar";
+	static final String BUTTON_CONTENT_3 = "Acelerar";
+	
+	//Colors
+	static final String COLOR_1 = "Red";
+	static final String COLOR_2 = "White";
+	
+	//booleans
+	static final String SPEEDUPENABLED = "true";
+	static final String SPEEDUPDISABLED= "false";
+	
+	
+	
+}
